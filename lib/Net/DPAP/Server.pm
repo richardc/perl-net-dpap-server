4fbf1795aad51d304b8441dea7669dce8d36f17a
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-07-14 15:08:43 +0000

    say hello
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Net-DPAP-Server@2640 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Net/DPAP/Server.pm
+++ lib/Net/DPAP/Server.pm
@@ -3,7 +3,18 @@ package Net::DPAP::Server;
 use base qw( Net::DAAP::Server );
 
 sub server_info {
- die "foo";
+    my $self = shift;
+    $self->_dmap_response( [[ 'dmap.serverinforesponse' => [
+        [ 'dmap.status' => 200 ],
+        [ 'dmap.protocolversion' => 2 ],
+        [ 'dpap.protocolversion' => 1 ],
+        [ 'dmap.itemname' => __PACKAGE__ ],
+        [ 'dmap.loginrequired' =>  0 ],
+        [ 'dmap.timeoutinterval' => 0 ],
+        [ 'dmap.supportsautologout' => 0 ],
+        [ 'dmap.authenticationmethod' => 0 ],
+        [ 'dmap.databasescount' => 1 ],
+       ]]] );
 }
 
 1;
