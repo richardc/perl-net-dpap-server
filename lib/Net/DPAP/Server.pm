6f7d387f39ae27f523ff51d1b41da478d5132100
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-07-16 16:56:20 +0000

    a dpap server!
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Net-DPAP-Server@2650 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Net/DPAP/Server.pm
+++ lib/Net/DPAP/Server.pm
@@ -21,13 +21,19 @@ sub server_info {
         [ 'dmap.status' => 200 ],
         [ 'dmap.protocolversion' => 2 ],
         [ 'dpap.protocolversion' => 1 ],
-        [ 'dmap.itemname' => __PACKAGE__ ],
+        [ 'dmap.itemname' => ref $self ],
         [ 'dmap.loginrequired' =>  0 ],
         [ 'dmap.timeoutinterval' => 0 ],
         [ 'dmap.supportsautologout' => 0 ],
         [ 'dmap.authenticationmethod' => 0 ],
         [ 'dmap.databasescount' => 1 ],
        ]]] );
+}
+
+sub always_answer {
+    my $self = shift;
+    return qw( dmap.itemid ) if $self->uri =~ /dpap.(thumb|hires)/;
+    return ( $self->SUPER::always_answer, 'dpap.imagefilename' );
 }
 
 1;
--- lib/Net/DPAP/Server/Image.pm
+++ lib/Net/DPAP/Server/Image.pm
@@ -2,6 +2,7 @@ package Net::DPAP::Server::Image;
 use strict;
 use warnings;
 use base qw( Class::Accessor::Fast );
+use File::Basename qw( basename );
 use Image::Info;
 use Perl6::Slurp;
 
@@ -9,7 +10,7 @@ __PACKAGE__->mk_accessors(qw(
       file
       dmap_itemid dmap_itemname dmap_itemkind dmap_containeritemid
 
-      dpap_aspectratio dpap_imagefilesize dpap_creationdate
+      dpap_imagefilename dpap_aspectratio dpap_imagefilesize dpap_creationdate
 ));
 
 sub new_from_file {
@@ -23,20 +24,29 @@ sub new_from_file {
     $self->file( $file );
     $self->dmap_itemid( $stat[1] ); # the inode should be good enough
     $self->dmap_containeritemid( 0+$self );
+
+    $self->dpap_imagefilename( basename $file );
     $self->dpap_aspectratio( $info->{width} / $info->{height} );
     $self->dpap_imagefilesize( $stat[7] );
     $self->dpap_creationdate( $stat[10] );
-    $self->dmap_itemname( $file );
+    $self->dmap_itemname( basename $file, qw( .jpeg .jpg ) );
     $self->dmap_itemkind( 3 );
 
     return $self;
 }
 
-sub dpap_picturedata {
+sub dpap_hires {
     my $self = shift;
-    slurp $self->file;
+    scalar slurp $self->file;
 }
 
-# A thumbnail should be 240x180 72dpi jpeg
+
+# A thumbnail should be smaller 240x180 72dpi jpeg
+
+sub dpap_thumb {
+    my $self = shift;
+    scalar slurp $self->file;
+}
+
 
 1;
